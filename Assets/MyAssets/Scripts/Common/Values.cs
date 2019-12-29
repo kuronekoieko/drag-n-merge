8d15f603ddb32047ac6829e72cd948e874336d4b
ieko <mendo931411.mou@gmail.com>
2019-12-29 19:07:53 +0900

    ブロック一括生成処理を実装

--- Assets/MyAssets/Scripts/Common/Values.cs
+++ Assets/MyAssets/Scripts/Common/Values.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public class Values : MonoBehaviour
 {
-   
+    public static readonly int BOARD_LENGTH_X = 6;
+    public static readonly int BOARD_LENGTH_Y = 9;
 }
--- Assets/MyAssets/Scripts/Game/BlocksManager.cs
+++ Assets/MyAssets/Scripts/Game/BlocksManager.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class BlocksManager : MonoBehaviour
 {
-    [SerializeField] BlockController[] blockControllers;
+    [SerializeField] BlockController blockControllerPrefab;
+    BlockController[] blockControllers;
     public void OnStart()
     {
-        for (int i = 0; i < blockControllers.Length; i++)
-        {
-            blockControllers[i].OnStart();
-        }
-
+        BlockGenerator();
     }
 
     public void OnUpdate()
@@ -19,6 +16,31 @@ public class BlocksManager : MonoBehaviour
         for (int i = 0; i < blockControllers.Length; i++)
         {
             blockControllers[i].OnUpdate();
+        }
+    }
+
+    void BlockGenerator()
+    {
+        int blockNum = Values.BOARD_LENGTH_X * Values.BOARD_LENGTH_Y;
+        blockControllers = new BlockController[blockNum];
+        Vector2 pos;
+        int i_x = 0;
+        int i_y = 0;
+        for (int i = 0; i < blockControllers.Length; i++)
+        {
+            pos.x = -2.47f + i_x * 0.16f * 6;
+            pos.y = -4.05f + i_y * 0.16f * 6;
+            Debug.Log(pos);
+            blockControllers[i] = Instantiate(blockControllerPrefab, pos, Quaternion.identity, transform);
+            blockControllers[i].OnStart();
+
+            i_x++;
+            if (i_x == Values.BOARD_LENGTH_X)
+            {
+                i_x = 0;
+                i_y++;
+            }
+
         }
     }
 }
