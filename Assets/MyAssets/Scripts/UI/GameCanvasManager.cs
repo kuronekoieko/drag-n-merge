a52ebeaf442f5d3ea65bcb3a76c7e56277481196
ieko <mendo931411.mou@gmail.com>
2019-12-31 03:50:04 +0900

    ターゲットの画像を変更

--- Assets/MyAssets/Scripts/UI/GameCanvasManager.cs
+++ Assets/MyAssets/Scripts/UI/GameCanvasManager.cs
@@ -11,11 +11,17 @@ public class GameCanvasManager : MonoBehaviour
     public void OnStart()
     {
         this.ObserveEveryValueChanged(timer => Variables.timer)
-            .Subscribe(timer => { timerText.text = timer.ToString("F2"); })
+            .Subscribe(timer => { SetTimeCountText(); })
             .AddTo(this.gameObject);
 
         targetNumText.text = Variables.targetNum.ToString();
     }
 
+    void SetTimeCountText()
+    {
+        int sec = Mathf.CeilToInt(Variables.timer);
+
+        timerText.text = Variables.timer.ToString("F2");
+    }
 
 }
