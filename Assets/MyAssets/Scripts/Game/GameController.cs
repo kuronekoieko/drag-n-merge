6a4a8eb4393ea544e1599d60592be37f2051188a
ieko <mendo931411.mou@gmail.com>
2019-12-31 05:52:10 +0900

    bundleidを設定

--- Assets/MyAssets/Scripts/Game/GameController.cs
+++ Assets/MyAssets/Scripts/Game/GameController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     [SerializeField] BlocksManager blocksManager;
-    float timeLimit = 50;
+    float timeLimit = 5;
     public void OnStart()
     {
         Variables.targetNum = 5;
