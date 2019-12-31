dc8d8dbefd51bcf417fe72c763eef22bc540e59e
ieko <mendo931411.mou@gmail.com>
2019-12-31 18:41:19 +0900

    ブロックの色データを15まで設定

---
+++ Assets/MyAssets/Scripts/Model/BlockColorData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MyGame/Create BlockColorData", fileName = "BlockColorData")]
+public class BlockColorData : ScriptableObject
+{
+    public BlockColor[] blockColors;
+}
+
+[System.Serializable]
+public class BlockColor
+{
+    public Color color;
+    public Color textColor;
+    public int num;
+}
