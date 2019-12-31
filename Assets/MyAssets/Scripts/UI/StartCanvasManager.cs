b3b6b63722000183c1737da4a4c9b2fac66d59f7
ieko <mendo931411.mou@gmail.com>
2019-12-31 20:57:39 +0900

    スタート画面のアニメーションを実装

--- Assets/MyAssets/Scripts/UI/StartCanvasManager.cs
+++ Assets/MyAssets/Scripts/UI/StartCanvasManager.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UniRx;
+using DG.Tweening;
 
 public class StartCanvasManager : MonoBehaviour
 {
     [SerializeField] Button startButton;
     [SerializeField] Text targetNumText;
     [SerializeField] Image targetBlockImage;
+    [SerializeField] Text startText;
 
     public void OnStart()
     {
@@ -17,6 +19,21 @@ public class StartCanvasManager : MonoBehaviour
         targetNumText.text = Variables.targetNum.ToString();
         targetNumText.color = BlockColorData.i.blockColors[Variables.targetNum - 1].textColor;
         targetBlockImage.color = BlockColorData.i.blockColors[Variables.targetNum - 1].color;
+
+        Anim();
+    }
+
+    void Anim()
+    {
+        startText.transform.DOScale(1.1f, 0.5f)
+               .OnComplete(() =>
+               {
+                   startText.transform.DOScale(1f, 0.5f)
+                           .OnComplete(() =>
+                           {
+                               Anim();
+                           });
+               });
     }
 
     void OnOpen()
