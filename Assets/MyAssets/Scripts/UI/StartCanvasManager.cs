62a5af410ac72ab364aeb66cab8b86adb4706c70
ieko <mendo931411.mou@gmail.com>
2019-12-31 19:52:21 +0900

    スタート画面にゴールを追加

--- Assets/MyAssets/Scripts/UI/StartCanvasManager.cs
+++ Assets/MyAssets/Scripts/UI/StartCanvasManager.cs
@@ -7,10 +7,15 @@ using UniRx;
 public class StartCanvasManager : MonoBehaviour
 {
     [SerializeField] Button startButton;
+    [SerializeField] Text targetNumText;
+    [SerializeField] Image targetBlockImage;
     public void OnStart()
     {
         startButton.onClick.AddListener(OnClickStartButton);
         gameObject.SetActive(true);
+        targetNumText.text = Variables.targetNum.ToString();
+        targetNumText.color = BlockColorData.i.blockColors[Variables.targetNum - 1].textColor;
+        targetBlockImage.color = BlockColorData.i.blockColors[Variables.targetNum - 1].color;
     }
 
     void OnOpen()
