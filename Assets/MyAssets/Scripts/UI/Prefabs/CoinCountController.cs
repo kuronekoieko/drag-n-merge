5556e697ac033f256bef2a57b45e47b3ee70bf38
家子智成 <>
2020-02-08 21:10:11 +0900

    【ゲーム画面】コイン表示に課金画面ボタンを追加

--- Assets/MyAssets/Scripts/UI/Prefabs/CoinCountController.cs
+++ Assets/MyAssets/Scripts/UI/Prefabs/CoinCountController.cs
@@ -7,10 +7,19 @@ using UniRx;
 public class CoinCountController : MonoBehaviour
 {
     [SerializeField] Text coinCountText;
+    [SerializeField] Button openShopButton;
+
     void Start()
     {
         this.ObserveEveryValueChanged(coinCount => SaveData.i.coinCount)
             .Subscribe(coinCount => coinCountText.text = coinCount.ToString())
             .AddTo(this.gameObject);
+
+        openShopButton.onClick.AddListener(OnClickShopButton);
+    }
+
+    void OnClickShopButton()
+    {
+
     }
 }
