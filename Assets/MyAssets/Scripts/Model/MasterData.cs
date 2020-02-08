ff4f5857ccadde0463a9ad0b7464be26e780722b
家子智成 <>
2020-02-09 00:20:42 +0900

    リソースファイルの場所を変更

---
+++ Assets/MyAssets/Scripts/Model/MasterData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterData : MonoBehaviour
+{
+    public int targetBlockCount;
+    public float timeLimit;
+    public int coinCount;
+}
