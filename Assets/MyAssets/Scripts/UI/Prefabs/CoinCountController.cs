be274a8e84dc6156731fe4148ba46997621c0e40
家子智成 <>
2020-02-08 21:02:36 +0900

    【ゲーム画面】コイン数表示を実装

---
+++ Assets/MyAssets/Scripts/UI/Prefabs/CoinCountController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class CoinCountController : MonoBehaviour
+{
+    [SerializeField] Text coinCountText;
+    void Start()
+    {
+        this.ObserveEveryValueChanged(coinCount => SaveData.i.coinCount)
+            .Subscribe(coinCount => coinCountText.text = coinCount.ToString())
+            .AddTo(this.gameObject);
+    }
+}
