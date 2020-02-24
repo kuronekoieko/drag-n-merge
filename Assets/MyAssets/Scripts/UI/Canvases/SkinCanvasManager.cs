2501fa641c86d00ad54c0aadef047233429782b3
家子智成 <>
2020-02-24 16:35:10 +0900

    【スキンボタン】トグルグループを設定

--- Assets/MyAssets/Scripts/UI/Canvases/SkinCanvasManager.cs
+++ Assets/MyAssets/Scripts/UI/Canvases/SkinCanvasManager.cs
@@ -6,21 +6,23 @@ using UniRx;
 
 public class SkinCanvasManager : BaseCanvasManager
 {
+    /*
+    【Unity uGUI】Toggleコンポーネントを徹底解説
+    https://tech.pjin.jp/blog/2017/01/29/unity_ugui_toggle/#Group
+    uGUIのScroll ViewでGrid
+    https://qiita.com/okuhiiro/items/d33a5c8940bf0843d8ff
+    */
     [SerializeField] Button closeButton;
-    [SerializeField] RectTransform content;
+    [SerializeField] ToggleGroup toggleGroup;
     [SerializeField] BackgroundToggleController backgroundToggleControllerPrefab;
     BackgroundToggleController[] bGToggleControllers;
+
     public override void OnStart()
     {
         base.SetScreenAction(thisScreen: ScreenState.SKIN);
         closeButton.onClick.AddListener(OnClickCloseButton);
 
-        bGToggleControllers = new BackgroundToggleController[BackgroundDataSO.i.backgroundDatas.Length];
-        for (int i = 0; i < bGToggleControllers.Length; i++)
-        {
-            bGToggleControllers[i] = Instantiate(backgroundToggleControllerPrefab, Vector3.zero, Quaternion.identity, content);
-            bGToggleControllers[i].OnStart(BackgroundDataSO.i.backgroundDatas[i]);
-        }
+        ToggleGanarator();
 
     }
 
@@ -32,6 +34,10 @@ public class SkinCanvasManager : BaseCanvasManager
     protected override void OnOpen()
     {
         gameObject.SetActive(true);
+        for (int i = 0; i < bGToggleControllers.Length; i++)
+        {
+            bGToggleControllers[i].isOn = (i == 0);
+        }
     }
 
     protected override void OnClose()
@@ -42,5 +48,16 @@ public class SkinCanvasManager : BaseCanvasManager
     void OnClickCloseButton()
     {
         Variables.screenState = ScreenState.START;
+    }
+
+    void ToggleGanarator()
+    {
+        RectTransform content = toggleGroup.GetComponent<RectTransform>();
+        bGToggleControllers = new BackgroundToggleController[BackgroundDataSO.i.backgroundDatas.Length];
+        for (int i = 0; i < bGToggleControllers.Length; i++)
+        {
+            bGToggleControllers[i] = Instantiate(backgroundToggleControllerPrefab, Vector3.zero, Quaternion.identity, content);
+            bGToggleControllers[i].OnStart(BackgroundDataSO.i.backgroundDatas[i], toggleGroup);
+        }
     }
 }
--- Assets/MyAssets/Scripts/UI/Prefabs/BackgroundToggleController.cs
+++ Assets/MyAssets/Scripts/UI/Prefabs/BackgroundToggleController.cs
@@ -9,14 +9,18 @@ public class BackgroundToggleController : MonoBehaviour
     [SerializeField] Image bGImage;
     [SerializeField] Text coinCountText;
     Toggle toggle;
+    public bool isOn
+    {
+        set { toggle.isOn = value; }
+        get { return toggle.isOn; }
+    }
 
-    public void OnStart(BackgroundData backgroundData)
+    public void OnStart(BackgroundData backgroundData, ToggleGroup toggleGroup)
     {
         bGImage.sprite = backgroundData.sprite;
         coinCountText.text = backgroundData.coinCount.ToString();
         toggle = GetComponent<Toggle>();
-
-
+        toggle.group = toggleGroup;
     }
 
 }
