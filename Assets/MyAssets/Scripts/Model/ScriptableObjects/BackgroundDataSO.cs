c7fa537b0d56dcb76540d9a4593851708ff34e32
家子智成 <>
2020-02-24 15:23:41 +0900

    【スキン変更画面】仮データ作成

---
+++ Assets/MyAssets/Scripts/Model/ScriptableObjects/BackgroundDataSO.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MyGame/Create BackgroundDataSO", fileName = "BackgroundDataSO")]
+public class BackgroundDataSO : ScriptableObject
+{
+    public BackgroundData[] backgroundDatas;
+
+    private static BackgroundDataSO _i;
+    public static BackgroundDataSO i
+    {
+        get
+        {
+            string PATH = "ScriptableObjects/" + nameof(BackgroundDataSO);
+            //初アクセス時にロードする
+            if (_i == null)
+            {
+                _i = Resources.Load<BackgroundDataSO>(PATH);
+
+                //ロード出来なかった場合はエラーログを表示
+                if (_i == null)
+                {
+                    Debug.LogError(PATH + " not found");
+                }
+            }
+
+            return _i;
+        }
+    }
+}
+
+[System.Serializable]
+public class BackgroundData
+{
+    public Sprite sprite;
+    public bool isTextBlack;
+    public int coinCount;
+}
