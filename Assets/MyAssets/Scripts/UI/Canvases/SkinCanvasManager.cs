797e8af54cee680c3cef57e9f1cea5ac54af8b88
家子智成 <>
2020-02-24 15:49:30 +0900

    【スキン画面】コンテンツを並べる設定

--- Assets/MyAssets/Scripts/UI/Canvases/SkinCanvasManager.cs
+++ Assets/MyAssets/Scripts/UI/Canvases/SkinCanvasManager.cs
@@ -7,10 +7,19 @@ using UniRx;
 public class SkinCanvasManager : BaseCanvasManager
 {
     [SerializeField] Button closeButton;
+    [SerializeField] RectTransform content;
+    [SerializeField] BackgroundToggleController backgroundToggleControllerPrefab;
+
     public override void OnStart()
     {
         base.SetScreenAction(thisScreen: ScreenState.SKIN);
         closeButton.onClick.AddListener(OnClickCloseButton);
+
+        for (int i = 0; i < BackgroundDataSO.i.backgroundDatas.Length; i++)
+        {
+            Instantiate(backgroundToggleControllerPrefab, Vector3.zero, Quaternion.identity, content);
+        }
+
     }
 
     public override void OnInitialize()
